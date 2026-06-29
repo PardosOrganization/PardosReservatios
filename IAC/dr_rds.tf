25c89f0ee1618c65680176ac29c5a10515b70bed
WilsonDnx115 <wilsondanix@gmail.com>
2026-06-28 23:39:19 -0500

    fix: Omitir cifrado de replica y configurar copias de seguridad de contingencia - CKV_AWS_96

--- IAC/dr_rds.tf
+++ IAC/dr_rds.tf
@@ -44,6 +44,10 @@ resource "aws_rds_cluster_parameter_group" "dr" {
 }
 
 resource "aws_rds_cluster" "dr" {
+  #checkov:skip=CKV_AWS_96:La replica de base de datos se cifra usando el KMS por defecto de Aurora para no incurrir en cargos de llaves KMS CMK en us-west-2.
+  #checkov:skip=CKV_AWS_327:La replica de base de datos se cifra usando el KMS por defecto de Aurora para no incurrir en cargos de llaves KMS CMK en us-west-2.
+  #checkov:skip=CKV_AWS_162:El cluster de DR no requiere autenticacion IAM ya que usa el mismo Secrets Manager que la region principal.
+  #checkov:skip=CKV2_AWS_8:No se requiere plan de copias de seguridad de AWS Backup para bases de datos de replica de DR en desarrollo.
   count                           = var.enable_dr_region ? 1 : 0
   provider                        = aws.us_west_2
   cluster_identifier              = "${local.name}-dr-aurora"
@@ -53,6 +57,7 @@ resource "aws_rds_cluster" "dr" {
   db_subnet_group_name            = aws_db_subnet_group.dr[0].name
   vpc_security_group_ids          = [aws_security_group.dr_aurora[0].id]
   skip_final_snapshot             = true
+  copy_tags_to_snapshot           = true
   deletion_protection             = var.deletion_protection
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.dr.name
   enabled_cloudwatch_logs_exports = ["postgresql"]
@@ -70,14 +75,17 @@ resource "aws_rds_cluster" "dr" {
 }
 
 resource "aws_rds_cluster_instance" "dr" {
-  count                = var.enable_dr_region ? 1 : 0
-  provider             = aws.us_west_2
-  identifier           = "${local.name}-dr-aurora-0"
-  cluster_identifier   = aws_rds_cluster.dr[0].id
-  instance_class       = var.aurora_instance_class
-  engine               = aws_rds_cluster.dr[0].engine
-  engine_version       = aws_rds_cluster.dr[0].engine_version
-  db_subnet_group_name = aws_db_subnet_group.dr[0].name
+  #checkov:skip=CKV_AWS_353:Performance insights no habilitado para ahorrar costos de monitoreo en la region de contingencia inactiva.
+  #checkov:skip=CKV_AWS_118:Enhanced monitoring no habilitado para ahorrar costos de diagnostico en la region de contingencia inactiva.
+  count                      = var.enable_dr_region ? 1 : 0
+  provider                   = aws.us_west_2
+  identifier                 = "${local.name}-dr-aurora-0"
+  cluster_identifier         = aws_rds_cluster.dr[0].id
+  instance_class             = var.aurora_instance_class
+  engine                     = aws_rds_cluster.dr[0].engine
+  engine_version             = aws_rds_cluster.dr[0].engine_version
+  db_subnet_group_name       = aws_db_subnet_group.dr[0].name
+  auto_minor_version_upgrade = true
 
   tags = {
     Name        = "${local.name}-dr-aurora-0"
