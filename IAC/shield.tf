151eea4f8c6f5c3f2f0d40050f11fc8004add177
WilsonDnx115 <wilsondanix@gmail.com>
2026-06-28 22:44:06 -0500

    refactor: Actualizacion de configuracion de proteccion Shield

--- IAC/shield.tf
+++ IAC/shield.tf
@@ -1,4 +1,5 @@
 resource "aws_shield_protection" "cloudfront" {
+  count        = var.enable_dr_region ? 1 : 0
   name         = "${local.name}-cf"
   resource_arn = aws_cloudfront_distribution.this.arn
   tags = {
